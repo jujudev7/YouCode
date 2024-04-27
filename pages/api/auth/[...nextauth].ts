ed76d82cd9573552ecc8f3a82da504a624464921
jujudev7 <penna.julien@gmail.com>
2024-04-27 20:43:56 +0200

    T1 - Setup application set-up finished

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,28 @@
+import { env } from '@/lib/env';
+import { prisma } from '@/lib/prisma';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import NextAuth, { AuthOptions } from 'next-auth';
+import { Adapter } from 'next-auth/adapters';
+import GithubProvider from 'next-auth/providers/github';
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
+  theme: {
+    logo: '/images/logo-text.png',
+  },
+  providers: [
+    GithubProvider({
+      clientId: env.GITHUB_ID,
+      clientSecret: env.GITHUB_SECRET,
+    }),
+  ],
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id;
+      session.user.image = user.image;
+      return session;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
