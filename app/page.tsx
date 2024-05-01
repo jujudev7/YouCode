1238c1306ede61b2f80bf61ddec7dd2fef922926
jujudev7 <penna.julien@gmail.com>
2024-05-01 23:44:09 +0200

    T6 - Add lessons list

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { Card, CardHeader, CardTitle } from '@/components/ui/card';
 
 export default function Home() {
   return (
-    <div>
-      <Card>
+    <div className='container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0'>
+      <Card className='mt-8'>
         <CardHeader>
           <CardTitle>Test card</CardTitle>
         </CardHeader>
--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -1,4 +1,4 @@
 export const SiteConfig = {
     title: 'YouCode',
-    description: 'You JUST need to CODE.',
+    description: "Apprends à coder plus vite grâce à une expérience d'apprentissage en ligne sans égal !",
   } as const;
